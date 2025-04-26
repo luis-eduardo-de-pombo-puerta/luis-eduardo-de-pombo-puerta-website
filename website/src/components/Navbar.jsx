b17300a97fcb6112bc6fd934fb9fd36f3bc8e7af
luis-eduardo-de-pombo-puerta <depombo2@gmail.com>
2025-04-26 14:32:53 -0500

    Update styling and layout changes

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
               {darkMode ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
             </button>
           </div>
-        </div>
+      </div>
       </div>
     </nav>
   );
